f2d8ed3100c25a5d2dbc9a6462139280ba12d87c
DhatriJangam <dhatrijangam602@gmail.com>
2024-07-24 09:36:58 +0530

    Create MovieDatabaseApp.java

---
+++ MovieDatabaseApp.java
@@ -0,0 +1,7 @@
+public class MovieDatabaseApp {
+    private static MovieDatabase movieDatabase = new MovieDatabase();
+
+    public static void main(String[] args) {
+        // CLI code to handle user inputs for adding, deleting, searching, and listing movies
+    }
+}
