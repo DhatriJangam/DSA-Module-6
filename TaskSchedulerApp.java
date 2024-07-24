028a7a6c3542ca1ed0cccaec51ead3dd45707400
DhatriJangam <dhatrijangam602@gmail.com>
2024-07-24 09:39:27 +0530

    Create TaskSchedulerApp.java

---
+++ TaskSchedulerApp.java
@@ -0,0 +1,7 @@
+public class TaskSchedulerApp {
+    private static TaskScheduler taskScheduler = new TaskScheduler();
+
+    public static void main(String[] args) {
+        // CLI code to handle user inputs for adding, deleting, searching, and listing tasks
+    }
+}
