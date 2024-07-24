45100f76c61972c5d213db52395e061e2a67f49f
DhatriJangam <dhatrijangam602@gmail.com>
2024-07-24 09:37:23 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    int id;
+    String description;
+    int priority;
+
+    public Task(int id, String description, int priority) {
+        this.id = id;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    // Getters and setters
+}
